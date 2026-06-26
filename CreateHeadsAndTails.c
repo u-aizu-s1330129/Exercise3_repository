23e57fca85d405a0680b5c0e830f0ba544818660
KHK-0617 <aoiosatomi@gmail.com>
2026-06-26 12:39:33 +0900

    HoT:initial revision

---
+++ CreateHeadsAndTails.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    int heads = 0;
+    int tails = 0;
+    int r;
+    int i;
+
+    /*Tossing a coin*/
+    printf("Tossing a coin...\n");
+    for(i=0;i<3;i++) {
+        r = rand() % 2;
+        if(r==0) {
+            heads++;
+            printf("Round %d: Heads\n", i+1);
+        } else {
+            tails++;
+            printf("Round %d: Tails\n", i+1);
+        }
+    }
+
+
+    return 0;
+}
